8291e2c7efe2d96474086bb3679861f9bcc31193
sman68jakarta <nadhiframadhan780@gmail.com>
2026-06-20 15:18:08 +0700

    Update APK download URL in initDownloadAPK function

--- sman68.js
+++ sman68.js
@@ -2036,7 +2036,7 @@ function closeCookieBanner() {
 function initDownloadAPK() {
     $('btnDownloadAPK')?.addEventListener('click', e => {
         e.preventDefault();
-        const url = 'https://github.com/nadhiframadhan780-dev/android.sman.68.jakarta/raw/refs/heads/main/SMAN%2068%20Jakarta.apk';
+        const url = 'https://www.sman68jakarta.xyz/SMAN%2068%20Jakarta.apk';
         window.open(url, '_blank');
     });
 }
